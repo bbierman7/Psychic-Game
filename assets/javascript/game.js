4df235e940bbd40fadef83038b416d39247e6240
Your Name <you@example.com>
2019-03-02 21:05:23 -0600

    updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+//Identify variables and set values
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesSoFar = //blank
+
+
+//Sequence of events:
+    //program generates random letter
+    //User presses letter on keyboard
+    //program accepts input and displays it after "Guesses so Far:_____" with a comma after 'var guess'
+
+//Conditions:
+    //if computer letter = user letter (ie input), add +1 to wins.
+        //set 'var guesses' back to = 9
+    if(guesses = ){
+
+    }
+
+    else(
+        {}
+    )
+        //if computer letter does not equal user input, add -1 to 'guesses'
+    
+        //if 'guesses' = 0, restart game
+        //set guesses to 9
+        // remove letters from 'guessesSoFar
+
+//Define variables
+
+
+
