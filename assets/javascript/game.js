37a3c2b27a572512af2c7923cf1fece0f75b65a1
Your Name <you@example.com>
2019-03-03 14:24:38 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,30 @@
-//Identify variables and set values
+// //Identify variables and set values
 var wins = 0;
 var losses = 0;
 var guesses = 9;
 var guessesSoFar = //blank
 
-
-
-function randomLetter(){
-    var x = document.getElementById
-    var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    var index = Math.floor((Math.random()*alphabet.length));
+// document.onkeypress = function(event){
+//     var key_press = String.fromCharCode(event.guesses);
+//     document.getElementById('guesses').innerHTML = guesses;
+//     var status = document.getElementById('status');
+//     status.innerHTML = "Your Guesses So far: " + key_press;
+// }
+// function myFunction() {
+// //     var x = 
+// //     var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+// //     var index = Math.floor((Math.random()*alphabet.length));
+// //     x.innerHTML = alphabet[index];
+// }
+function letters() {
+    var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var index = Math.floor((Math.random() * alphabet.length));
     x.innerHTML = alphabet[index];
-}
-console.log(randomLetter);
+  }
 
+function keyPressed(event) {
+    var x = event.alphabet;
+    document.getElementById("guesses").innerHTML = "Your guesses so far: " + x;
+  }
 
-
+ 
