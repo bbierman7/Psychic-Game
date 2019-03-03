6bb22b704ac31933d41e11b280159c60034341a6
Your Name <you@example.com>
2019-03-03 11:23:19 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,28 +5,14 @@ var guesses = 9;
 var guessesSoFar = //blank
 
 
-//Sequence of events:
-    //program generates random letter
-    //User presses letter on keyboard
-    //program accepts input and displays it after "Guesses so Far:_____" with a comma after 'var guess'
 
-//Conditions:
-    //if computer letter = user letter (ie input), add +1 to wins.
-        //set 'var guesses' back to = 9
-    if(guesses = ){
-
-    }
-
-    else(
-        {}
-    )
-        //if computer letter does not equal user input, add -1 to 'guesses'
-    
-        //if 'guesses' = 0, restart game
-        //set guesses to 9
-        // remove letters from 'guessesSoFar
-
-//Define variables
+function randomLetter(){
+    var x = document.getElementById
+    var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    var index = Math.floor((Math.random()*alphabet.length));
+    x.innerHTML = alphabet[index];
+}
+console.log(randomLetter);
 
 
 
