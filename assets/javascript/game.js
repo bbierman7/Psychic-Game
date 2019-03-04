8223269c916b3edd6e1e7051d2689322a5b9abdb
bbierman <bbierman7@gmail.com>
2019-03-03 20:22:31 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,27 @@
-// //Identify variables and set values
-var wins = 0;
-var losses = 0;
-var guesses = 9;
-var guessesSoFar = //blank
-
-// document.onkeypress = function(event){
-//     var key_press = String.fromCharCode(event.guesses);
-//     document.getElementById('guesses').innerHTML = guesses;
-//     var status = document.getElementById('status');
-//     status.innerHTML = "Your Guesses So far: " + key_press;
-// }
-// function myFunction() {
-// //     var x = 
-// //     var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-// //     var index = Math.floor((Math.random()*alphabet.length));
-// //     x.innerHTML = alphabet[index];
-// }
+//Computer generates random letter
 function letters() {
     var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     var index = Math.floor((Math.random() * alphabet.length));
     x.innerHTML = alphabet[index];
   }
 
+//User enters letter
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesSoFar=[]
+document.onkeyup=function(event){
+
+})
+
 function keyPressed(event) {
     var x = event.alphabet;
     document.getElementById("guesses").innerHTML = "Your guesses so far: " + x;
   }
 
+//Print user guess next to 'Guesses so far'. Every letter should have a comma next to it.
+
  
+
+//If user letter matches computer letter, add +1 to wins. Else, add +1 to Losses
+
