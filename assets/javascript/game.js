f66e0fafdac22f6e5d3104fbee6bea312974f6e9
bbierman <bbierman7@gmail.com>
2019-03-04 19:59:21 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,35 +2,60 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var userGuess=[,];
+var userGuess = [,];
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var randomLetter;
+var userGet =
 
 
-//Functions:
+  //Functions:
 
-    //Computer generates random letter
-    function letters() {
-      var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-      var index = Math.floor((Math.random() * alphabet.length));
-      x.innerHTML = alphabet[index];
-      console.log();
+  //Computer generates random letter
+  function letters() {
+    randomLetter = alphabet[Math.floor((Math.random() * alphabet.length))]
+  }
+
+//User presses any key
+function userGuess(event) {
+  event.key;
+}
+
+document.addEventListener("keyup", function (event) {
+  console.log(event.key);
+
+  if (randomLetter === event.key) {
+    wins++;
+    document.getElementById("wins").innerHTML = wins;
+    guessesLeft = 9;
+    document.getElementById("guessesLeft").innerHTML = guessesLeft;
+    randomLetter = alphabet[Math.floor((Math.random() * alphabet.length))]
+  } else {
+    if(guessesLeft=0){
+    guessesLeft--;
+    document.getElementById("guessesLeft").innerHTML = guessesLeft;
+    }
+  }
+})
+
+//Tells user how many guesses they have left
+function guessesLeft() {
+  var i;
+  for (i = 9; i <= guessesLeft; i--) {
+
+    if (userGuess === letters) {
+      wins++ , guessesLeft === 9 //add +1 to wins. Reset guessesLeft to 9
     }
 
-    //User presses any key
-      function userGuess(event){
-        var x = event.key;
-      }
-
-    function guessesLeft(){
-          var i;
-          for(i = 9; i <= guessesLeft; i--){
-              if(userGuess === letters){
-                wins++, guessesLeft === 9 //add +1 to wins. Reset guessesLeft to 9
-                console.log()
-              }
-              else{
-                losses++, guessesLeft--
-                console.log()
-              }
-          }
+    else {
+      losses++ , guessesLeft--
     }
+  }
+}
+
+          // document.getEklementById("wins").innerHTML = "You won! " + wins;
+          // document.getElementById("losses").innerHTML = "Losses: " + losses;
+          // document.getElementById("guessesLeft").innerHTML = "Guesses left: " + guessesLeft;   
+          // console.log();         
+          // document.getElementById("userGuess").innerHTML = "Your guesses so far: " + userGuess;
+          // console.log();
 
